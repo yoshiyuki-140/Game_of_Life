3d7a2fc551a59012c4101bd09d010227faaecc1b
yoshiyuki-140 <moyashikuro1225@gmail.com>
2022-10-15 22:00:28 +0900

    create objects file for use command line mode

---
+++ src/commandmode.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+import pygame
+
+
+class CommandUtil:
+    def __init__(self) -> None:
+        self.screen = pygame.display.get_surface()
+        self.window = self.screen.get_rect()
+        pass
+
+    def startCommandMode(self):
+        pass
+    
