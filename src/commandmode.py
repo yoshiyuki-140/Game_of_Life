65c78cf6d49a0e81d180d363aaef91916ef69295
yoshiyuki-140 <moyashikuro1225@gmail.com>
2022-10-24 15:32:21 +0900

    add commandmode.py ;

--- src/commandmode.py
+++ src/commandmode.py
@@ -1,21 +1,29 @@
 #coding:utf-8
 
 import pygame
+from readchar import readkey, key
 
 
 class CommandUtil:
     def __init__(self) -> None:
         self.screen = pygame.display.get_surface()
         self.window = self.screen.get_rect()
-        self.textSize = 50
+        self.command = str(':')
 
-    def startCommandMode(self):
-        self.commandInput()
+    def returnCommandMode(self, commands:list):
+        while True:
+            k = readkey()
+            self.command += k
+            self.blitCommand()
+            if k == key.ENTER:
+                if self.command in commands:
+                    return self.command
 
-    def commandInput(self):
-        self.inStr = input(":")
-        self.commandStr = pygame.font.SysFont(
-            None, self.textSize).render(self.string, True, (0, 0, 0))
-        self.screen.blit(self.commandStr, (self.window.centerx - self.commandStr.get_width() //
-                                           2, self.window.bottom - self.commandStr.get_height() - 10))
-        
+            if k == key.BACKSPACE:
+                self.command = self.command[:-2]  # commandの最後の文字を削除
+                self.blitCommand()
+    def blitCommand(self):
+        font = pygame.font.SysFont(None,50)
+        cmdMsg = font.render(self.command,True,(0,0,0))
+        self.screen.blit(cmdMsg,0,self.window.bottom - cmdMsg.get_rect().height - 10)
+        pygame.display.update()
